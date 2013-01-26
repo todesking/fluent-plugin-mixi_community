e7adbfa5333370aa7402e85efc661997b9101d3e
todesking <discommunicative@gmail.com>
2013-01-27 02:54:47 +0900

    Diff update, recent_threads_num

--- lib/fluent/plugin/in_mixi_community.rb
+++ lib/fluent/plugin/in_mixi_community.rb
@@ -1,18 +1,31 @@
+require 'fluent/plugin'
+require 'fluent/config'
+require 'fluent/input'
+
+1.tap do
+  # https://github.com/fluent/fluentd/issues/76
+  encoding = Encoding.default_internal
+  Encoding.default_internal = nil
+  require 'mime/types'
+  Encoding.default_internal = encoding
+end
+
 require 'mixi-community'
 require 'pit'
 
 class Fluent::MixiCommunityInput < Fluent::Input
-  Fluent::Plugin.register_output('mixi_community', self)
+  Fluent::Plugin.register_input('mixi_community', self)
 
   config_param :interval_sec, :integer
   config_param :pit_id, :string
   config_param :community_id, :integer
-  config_param :title_pattern, :string
+  config_param :thread_title_pattern, :string
+  config_param :recent_threads_num, :integer
   config_param :tag, :string
 
   def configure(config)
     super
-    @title_pattern = Regexp.new(@title_pattern)
+    @thread_title_pattern = Regexp.new(@thread_title_pattern, {}, 'n')
 
     user_info = Pit.get(@pit_id, require: {
       'email' => 'mail',
@@ -23,6 +36,8 @@ class Fluent::MixiCommunityInput < Fluent::Input
       user_info['password']
     )
     @community = Mixi::Community.new(@community_id)
+    # {[community_id, bbs_id] => last_comment_id}
+    @last_comment_ids = {}
   end
 
   def start
@@ -42,9 +57,14 @@ class Fluent::MixiCommunityInput < Fluent::Input
 
   def fetch_and_emit
     @community.fetch(@fetcher)
-    @community.recent_bbses.select{|th| th.title =~ @title_pattern}.each do|th|
+    @community.recent_bbses[0...@recent_threads_num].select{|th| th.title =~ @thread_title_pattern}.each do|th|
+      sleep 1
       th.fetch(@fetcher)
       th.recent_comments.each do|comment|
+        last_comment_id = @last_comment_ids[[@community.id, th.id]]
+        next if last_comment_id && comment.id <= last_comment_id
+        @last_comment_ids[[@community.id, th.id]] = comment.id
+
         Fluent::Engine.emit(@tag, Fluent::Engine.now, {
           'community' => {
             'id' => @community.id,
